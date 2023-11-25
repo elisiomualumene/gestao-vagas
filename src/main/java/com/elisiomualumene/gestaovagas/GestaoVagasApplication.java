e417e99da7c3acbdc374c9a52be3379a9bf88e05
elisiomualumene <elisiomualumene@gmail.com>
2023-11-25 18:02:06 +0100

    :started project :fire:

---
+++ src/main/java/com/elisiomualumene/gestaovagas/GestaoVagasApplication.java
@@ -0,0 +1,13 @@
+package com.elisiomualumene.gestaovagas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoVagasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoVagasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elisiomualumene/gestaovagas/GestaoVagasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elisiomualumene.gestaovagas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoVagasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
