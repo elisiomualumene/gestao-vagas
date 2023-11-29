4f452185699b9fc63acf8d15176f61ce7178dc1d
elisiomualumene <elisiomualumene@gmail.com>
2023-11-29 22:02:38 +0100

    update SecurityConfig.java

--- src/main/java/com/elisiomualumene/gestao_vagas/security/SecurityConfig.java
+++ src/main/java/com/elisiomualumene/gestao_vagas/security/SecurityConfig.java
@@ -1,24 +1,31 @@
 package com.elisiomualumene.gestao_vagas.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+
+    @Autowired
+    private SecurityFilter securityFilter;
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> {
                     auth.requestMatchers("/candidate/").permitAll()
                             .requestMatchers("/company/").permitAll()
-                            .requestMatchers("/auth/company").permitAll();
+                            .requestMatchers("/company/auth").permitAll()
+                            .requestMatchers("/auth/candidate").permitAll();
                     auth.anyRequest().authenticated();
 
-                });
+                }).addFilterBefore(securityFilter, BasicAuthenticationFilter.class);
 
         return http.build();
     }
