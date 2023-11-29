bf4bfb06eeb94b358c1639e5b267380c3c2e2c74
elisiomualumene <elisiomualumene@gmail.com>
2023-11-29 22:02:31 +0100

    update AuthCompanyUseCase.java

--- src/main/java/com/elisiomualumene/gestao_vagas/modules/company/usecases/AuthCompanyUseCase.java
+++ src/main/java/com/elisiomualumene/gestao_vagas/modules/company/usecases/AuthCompanyUseCase.java
@@ -1,5 +1,9 @@
 package com.elisiomualumene.gestao_vagas.modules.company.usecases;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Arrays;
+
 import javax.security.sasl.AuthenticationException;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +15,7 @@ import org.springframework.stereotype.Service;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.elisiomualumene.gestao_vagas.modules.company.dto.AuthCompanyDTO;
+import com.elisiomualumene.gestao_vagas.modules.company.dto.ResponseCompanyToken;
 import com.elisiomualumene.gestao_vagas.modules.company.repository.CompanyRepository;
 
 @Service
@@ -23,9 +28,9 @@ public class AuthCompanyUseCase {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-    public String execute(AuthCompanyDTO authCompanyDTO) throws AuthenticationException {
+    public ResponseCompanyToken execute(AuthCompanyDTO authCompanyDTO) throws AuthenticationException {
         var company = companyRepository.findByUsername(authCompanyDTO.getUsername()).orElseThrow(() -> {
-            throw new UsernameNotFoundException("Usuário não existe");
+            throw new UsernameNotFoundException("Usuário ou Senha Incorretos");
         });
 
         var passwordMatcher = passwordEncoder.matches(authCompanyDTO.getPassword(), company.getPassword());
@@ -33,9 +38,18 @@ public class AuthCompanyUseCase {
         if (!passwordMatcher) {
             throw new AuthenticationException("Senha incorreta!");
         }
-        Algorithm algorithm = Algorithm.HMAC256(secretKey);
-        var token = JWT.create().withIssuer("java").withSubject(company.getId().toString()).sign(algorithm);
 
-        return token;
+        var expires_In = Instant.now().plus(Duration.ofHours(2));
+
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+        
+        var token = JWT.create().withIssuer("java").withExpiresAt(Instant.now().plus(Duration.ofHours(2)))
+                .withClaim("roles", Arrays.asList("COMPANY")).withExpiresAt(expires_In)
+                .withSubject(company.getId().toString()).sign(algorithm);
+
+        var responseCompanyToken = ResponseCompanyToken.builder().token(token).expires_in(expires_In.toEpochMilli())
+                .build();
+
+        return responseCompanyToken;
     }
 }
