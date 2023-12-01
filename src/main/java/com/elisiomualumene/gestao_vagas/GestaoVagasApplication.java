1151882d3ba4c54cb62aebe4d20f20f33cdc7d15
elisiomualumene <elisiomualumene@gmail.com>
2023-12-01 17:35:48 +0100

    update pom.xml and GestaoVagasApplication.java

--- src/main/java/com/elisiomualumene/gestao_vagas/GestaoVagasApplication.java
+++ src/main/java/com/elisiomualumene/gestao_vagas/GestaoVagasApplication.java
@@ -3,7 +3,11 @@ package com.elisiomualumene.gestao_vagas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Gestão de Vagas", description = "API Responsável pela Gestão de Vagas", version = "1"))
 public class GestaoVagasApplication {
 
 	public static void main(String[] args) {
