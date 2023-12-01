36e3485a0af4866972d5f5d63ea9779bd6deb3f9
elisiomualumene <elisiomualumene@gmail.com>
2023-12-01 17:35:52 +0100

    update JobController.java

--- src/main/java/com/elisiomualumene/gestao_vagas/modules/company/controllers/JobController.java
+++ src/main/java/com/elisiomualumene/gestao_vagas/modules/company/controllers/JobController.java
@@ -4,9 +4,9 @@ import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.elisiomualumene.gestao_vagas.modules.company.entities.JobEntity;
 import com.elisiomualumene.gestao_vagas.modules.company.usecases.CreateJobUseCase;
+import com.elisiomualumene.gestao_vagas.modules.company.usecases.ListJobUseCase;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
@@ -23,6 +24,9 @@ import jakarta.validation.Valid;
 public class JobController {
     @Autowired
     private CreateJobUseCase createJobUseCase;
+
+    @Autowired
+    private ListJobUseCase listJobUseCase;
 
     @PostMapping("/")
     @PreAuthorize("hasRole('COMPANY')")
@@ -34,6 +38,17 @@ public class JobController {
             return ResponseEntity.ok().body(job);
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/")
+    @PreAuthorize("hasRole('COMPANY')")
+    public ResponseEntity<Object> listJobs() {
+        try {
+            var jobs = this.listJobUseCase.execute();
+            return ResponseEntity.ok().body(jobs);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
 }
