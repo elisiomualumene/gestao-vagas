4e94868982980d4900e1b04d04702d64cc394981
elisiomualumene <elisiomualumene@gmail.com>
2023-12-02 11:02:09 +0100

    update AuthCompany.java

--- src/main/java/com/elisiomualumene/gestao_vagas/modules/company/controllers/AuthCompany.java
+++ src/main/java/com/elisiomualumene/gestao_vagas/modules/company/controllers/AuthCompany.java
@@ -12,16 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.elisiomualumene.gestao_vagas.modules.company.dto.AuthCompanyDTO;
 import com.elisiomualumene.gestao_vagas.modules.company.usecases.AuthCompanyUseCase;
-import com.elisiomualumene.gestao_vagas.utils.CustomResponse;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/company")
+@Tag(name = "Autenticação da Empresa")
 public class AuthCompany {
 
     @Autowired
     private AuthCompanyUseCase authCompanyUseCase;
 
     @PostMapping("/auth")
+    @Operation(summary = "Autenticação da empresa usando username e senha")
     public ResponseEntity<Object> authCompany(@RequestBody AuthCompanyDTO authCompanyDTO)
             throws AuthenticationException {
         try {
