1697202d92390417ea62133fdd1bafaddc8bf52c
elisiomualumene <elisiomualumene@gmail.com>
2023-12-02 11:02:26 +0100

    update JobEntity.java

--- src/main/java/com/elisiomualumene/gestao_vagas/modules/company/entities/JobEntity.java
+++ src/main/java/com/elisiomualumene/gestao_vagas/modules/company/entities/JobEntity.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -13,25 +14,33 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Data
 @Entity(name = "job")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class JobEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-    @NotBlank
+    @Schema(example = "Vaga para design")
     private String description;
 
+    @Schema(example = "GymPass, Plano de saúde")
     private String benefits;
 
-    @NotBlank
+    @NotBlank(message = "Esse campo é obrigatório")
+    @Schema(example = "SENIOR")
     private String level;
 
-    @ManyToOne
+    @ManyToOne()
     @JoinColumn(name = "company_id", insertable = false, updatable = false)
     private CompanyEntity companyEntity;
 
@@ -40,8 +49,4 @@ public class JobEntity {
 
     @CreationTimestamp
     private LocalDateTime createdAt;
-
-    public static Object builder() {
-        return null;
-    }
 }
