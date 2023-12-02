2f346347e0e1f5c5e2e2d89c2035eec2ef56e240
elisiomualumene <elisiomualumene@gmail.com>
2023-12-02 11:02:30 +0100

    update JobRepository.java

--- src/main/java/com/elisiomualumene/gestao_vagas/modules/company/repository/JobRepository.java
+++ src/main/java/com/elisiomualumene/gestao_vagas/modules/company/repository/JobRepository.java
@@ -1,5 +1,6 @@
 package com.elisiomualumene.gestao_vagas.modules.company.repository;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,5 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.elisiomualumene.gestao_vagas.modules.company.entities.JobEntity;
 
 public interface JobRepository extends JpaRepository<JobEntity, UUID> {
-
+    List<JobEntity> findAllByCompanyId(UUID companyId);
 }
