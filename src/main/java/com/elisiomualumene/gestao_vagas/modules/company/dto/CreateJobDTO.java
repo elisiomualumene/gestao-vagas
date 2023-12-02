3a5981f215d95b542885755d597cad5c067cfdd3
elisiomualumene <elisiomualumene@gmail.com>
2023-12-02 11:02:21 +0100

    update CreateJobDTO.java

--- src/main/java/com/elisiomualumene/gestao_vagas/modules/company/dto/CreateJobDTO.java
+++ src/main/java/com/elisiomualumene/gestao_vagas/modules/company/dto/CreateJobDTO.java
@@ -1,5 +1,7 @@
 package com.elisiomualumene.gestao_vagas.modules.company.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,7 +12,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CreateJobDTO {
+    @Schema(example = "Vaga para desenvolvedor de Software", requiredMode = RequiredMode.REQUIRED)
     private String description;
+
+    @Schema(example = "Plano de Saude, GYM Pass", requiredMode = RequiredMode.REQUIRED)
     private String benefits;
+
+    @Schema(example = "SENIOR", requiredMode = RequiredMode.REQUIRED)
     private String level;
 }
