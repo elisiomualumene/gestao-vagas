f81d9fc29d6200df7adf2ce9cffe5fed72b738d1
elisiomualumene <elisiomualumene@gmail.com>
2023-12-02 11:02:34 +0100

    update CreateJobUseCase.java

--- src/main/java/com/elisiomualumene/gestao_vagas/modules/company/usecases/CreateJobUseCase.java
+++ src/main/java/com/elisiomualumene/gestao_vagas/modules/company/usecases/CreateJobUseCase.java
@@ -3,16 +3,24 @@ package com.elisiomualumene.gestao_vagas.modules.company.usecases;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.elisiomualumene.gestao_vagas.exceptions.FoundException;
 import com.elisiomualumene.gestao_vagas.modules.company.entities.JobEntity;
+import com.elisiomualumene.gestao_vagas.modules.company.repository.CompanyRepository;
 import com.elisiomualumene.gestao_vagas.modules.company.repository.JobRepository;
 
 @Service
 public class CreateJobUseCase {
+
     @Autowired
     private JobRepository jobRepository;
 
-    public JobEntity execute(JobEntity jobEntity) {
-       return this.jobRepository.save(jobEntity);
+    @Autowired
+    private CompanyRepository companyRepository;
 
+    public JobEntity execute(JobEntity jobEntity) {
+        companyRepository.findById(jobEntity.getCompanyId()).orElseThrow(() -> {
+            throw new FoundException("Empresa não encontrada");
+        });
+        return this.jobRepository.save(jobEntity);
     }
 }
